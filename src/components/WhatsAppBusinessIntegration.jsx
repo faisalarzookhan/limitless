6c9ff487c21120f8ed45a9bfc9887d4b5a158b70
Wakilni Developer <developer@wakilni.com>
2026-01-06 18:21:26 +0300

    LIS v3.9.1

--- src/components/WhatsAppBusinessIntegration.jsx
+++ src/components/WhatsAppBusinessIntegration.jsx
@@ -116,7 +116,7 @@ const WhatsAppBusinessIntegration = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-6 left-6 z-50">
       {!isOpen ? (
         <button
           onClick={() => setIsOpen(true)}
